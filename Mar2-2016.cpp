4e0f092ed2c6f70c87631968883b3b1761fcf007
Dpasi314 <dante.pasionek1@gmail.com>
2016-03-02 15:20:24 -0700

    Notes on deleting nodes from a BST (Cont)

---
+++ Mar2-2016.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	return 0;
+}
+
+
+/*
+Notes on Tree Traversal
+f 2 children
+	node->parent->leftChild = min
+	Node->leftChild0>parent = min
+	min->parent = node->parent
+	min->leftChild = node->leftChild
+
+
+
+*/
